cc22ef95951c6cfbcbcb240c2ef6c7e39778e784
Vincent Salamand <salamandvincent@gmail.com>
2017-08-28 16:17:52 +0200

    redoing github

---
+++ lib/bot.rb
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+require 'recastai'
+require 'dotenv/load'
+require 'pry'
+require 'json'
+require 'open-uri'
+
+
+def bot(payload)
+  connect = RecastAI::Connect.new(ENV['REQUEST_TOKEN'], ENV['LANGUAGE'])
+  request = RecastAI::Request.new(ENV['REQUEST_TOKEN'])
+
+  connect.handle_message(payload) do |message|
+    response = request.converse_text(message.content, conversation_token: message.sender_id)
+
+    if response.intent.slug == "reco_recette"
+      url = 'http://localhost:3000/api/v1/suggest'
+      suggest_serialized = open(url).read
+      suggest = JSON.parse(suggest_serialized)
+
+
+      contents = []
+      suggest["recipes"].each do |recipe|
+              contents << {
+                title: "#{recipe["title"]}",
+                imageUrl: "#{recipe["imageUrl"]}",
+                buttons: [
+                  {
+                    title: 'Voir la recette',
+                    value: "http://localhost:3000#{recipe['recipeUrl']}",
+                    type: 'web_url',
+                  }
+                ]
+              }
+      end
+      messages = [
+        {
+          type: 'carousel',
+          content: contents,
+        }
+      ]
+      connect.send_message(messages, message.conversation_id)
+    else
+      replies = response.replies.map{ |r| { type: 'text', content: r } }
+      connect.send_message(replies, message.conversation_id)
+    end
+
+  end
+
+  200
+end
