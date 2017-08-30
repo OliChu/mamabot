798399033384b5710b85d9e0323e1a2342f5db08
Vincent Salamand <salamandvincent@gmail.com>
2017-08-30 12:37:48 +0200

    lots of cool stuffs

--- lib/bot.rb
+++ lib/bot.rb
@@ -9,37 +9,54 @@ require 'open-uri'
 
 def bot(payload)
   connect = RecastAI::Connect.new(ENV['REQUEST_TOKEN'], ENV['LANGUAGE'])
-  request = RecastAI::Request.new(ENV['REQUEST_TOKEN'])
+  request = RecastAI::Request.new(ENV['REQUEST_TOKEN'], ENV['LANGUAGE'])
 
   connect.handle_message(payload) do |message|
     response = request.converse_text(message.content, conversation_token: message.sender_id)
+    username = URI.escape(message.message["data"]["userName"])
+    sender_id = message.sender_id
 
-    if message.type == "payload" && (message.content.include? "search?ingredients")
-      messages = select_food(message)
-      connect.send_message(messages, message.conversation_id)
-    elsif response.intent.slug
-      if response.intent.slug == "reco_recette" || message.content == "SUGGEST_RECIPE"
-        username = URI.escape(message.message["data"]["userName"])
-        sender_id = message.sender_id
-        messages = send_suggestions(username, sender_id)
-        connect.send_message(messages, message.conversation_id)
-        # messages = suggest_quick_replies
-        # connect.send_message(messages, message.conversation_id)
+    unless response.intent.nil?
 
-      elsif response.intent.slug == "recette-ingredients"
-        query = []
-        ingredients = response.entities.select { |entity| entity.name == "ingredient" }
-        ingredients.each { |entity| query << "ingredients[]=#{entity.value}" }
-        query = query.join("&")
-        messages = search_food(query)
-        connect.send_message(messages, message.conversation_id)
+      if response.intent.slug
+        if response.intent.slug == "suggest-food"
+          messages = send_suggestions(username, sender_id)
+          connect.send_message(messages, message.conversation_id)
 
-      else
-        replies = response.replies.map{ |r| { type: 'text', content: r } }
-        connect.send_message(replies, message.conversation_id)
+        elsif response.intent.slug == "food-history"
+          messages = send_history(username, sender_id)
+          connect.send_message(messages, message.conversation_id)
+
+        elsif response.intent.slug == "search-ingredients"
+          query = []
+          ingredients = response.entities.select { |entity| entity.name == "ingredient" }
+          emojis = response.entities.select { |entity| entity.name == "emoji" }
+          if (ingredients.any?)
+            ingredients.each { |entity| query << "ingredients[]=#{entity.value}" }
+            query = query.join("&")
+            messages = search_food(query)
+            connect.send_message(messages, message.conversation_id)
+          elsif (emojis.any?)
+            emojis.each { |entity| query << "ingredients[]=#{entity.description}" }
+            query = query.join("&")
+            messages = search_food(query)
+            connect.send_message(messages, message.conversation_id)
+          end
+
+        elsif response.intent.slug == "search-by-id"
+          messages = select_food(response.get_memory('recette_id').value.gsub(/[^0-9,.]/, ""), username, sender_id)
+          connect.send_message(messages, message.conversation_id)
+
+        else
+          replies = response.replies.map{ |r| { type: 'text', content: r } }
+          connect.send_message(replies, message.conversation_id)
+        end
       end
-    end
 
+    else
+      replies = response.replies.map{ |r| { type: 'text', content: r } }
+      connect.send_message(replies, message.conversation_id)
+    end
   end
   200
 end
@@ -62,25 +79,34 @@ def send_suggestions(username, sender_id)
               {
                 type: 'postback',
                 title: 'Faire cette recette',
-                value: "http://www.foodmama.fr/api/v1/search?ingredients[]=#{recipe["title"]}"
+                value: "Je cherche la recette id_#{recipe['recipeId']}"
               }
             ]
           }
   end
-  if content.blank?
-     messages = [
+  messages = [
+    {
+      type: 'carousel',
+      content: content
+    },
+    {
+      type: 'quickReplies',
+      content:
       {
-        type: 'text',
-        content: "Oops, Mama n'a rien pour cette recherche"
-      }
-    ]
-  else messages = [
+        title: "...",
+        buttons: [
           {
-            type: 'carousel',
-            content: content
+            title: 'Autres suggestions ?',
+            value: 'Donnes-moi des idées'
+          },
+          {
+            title: 'Demander à Mama',
+            value: 'Je veux chercher une recette'
           }
         ]
-  end
+      }
+    }
+  ]
 end
 
 def search_food(query)
@@ -101,50 +127,130 @@ def search_food(query)
               {
                 type: 'postback',
                 title: 'Faire cette recette',
-                value: "http://www.foodmama.fr/api/v1/search?ingredients[]=#{recipe["title"]}"
+                value: "Je cherche la recette id_#{recipe['recipeId']}"
               }
             ]
           }
   end
-  messages = [
+  if content.empty?
+     messages = [
+      {
+        type: 'text',
+        content: "Oops, Mama n'a pas ce que tu demandes 😵"
+      },
+      {
+        type: 'quickReplies',
+        content:
+        {
+          title: "...",
+          buttons: [
+            {
+              title: 'Des suggestions ?',
+              value: 'Donnes-moi des idées'
+            },
+            {
+              title: 'Chercher ?',
+              value: 'Je veux chercher une recette'
+            }
+          ]
+        }
+      }
+    ]
+  else messages = [
+          {
+            type: 'carousel',
+            content: content
+          }
+        ]
+  end
+end
+
+def select_food(recipeId, username, sender_id)
+  url = "https://www.foodmama.fr/api/v1/select?recipe=#{recipeId}&sender_id=#{sender_id}&userName=#{username}"
+  selected_food_serialized = open(url).read
+  selected_food = JSON.parse(selected_food_serialized)
+  selected_food_ingredients = selected_food["ingredients"].map { |dose| "* #{dose["dose"]} #{dose["ingredient"]} #{dose["complement"]}"}.join("\n")
+  return messages = [
     {
-      type: 'carousel',
-      content: content
+      type: 'text',
+      content: selected_food["title"] + ":\n(🍴 2 pers.)\n" + selected_food_ingredients,
+    },
+    {
+      type: 'quickReplies',
+      content:
+      {
+        title: "Besoin d'autre chose?",
+        buttons: [
+          {
+            title: 'Non merci !',
+            value: 'merci Mama'
+          },
+         {
+            title: 'Suggestions ?',
+            value: 'Donnes-moi des idées'
+          },
+          {
+            title: 'Chercher ?',
+            value: 'Je veux chercher une recette'
+          }
+        ]
+      }
     }
   ]
 end
 
-# def suggest_quick_replies
-#   messages = [
-#     {
-#       type: 'quickReplies',
-#       content: {
-#         title: '',
-#         buttons: [
-#           {
-#             title: 'Autres suggestions',
-#             value: 'SUGGEST_RECIPE',
-#           }, {
-#             title: 'Chercher',
-#             value: 'SUGGEST_RECIPE',
-#           }
-#         ]
-#       }
-#     }
-#   ]
-# end
-
-def select_food(message)
-  url = URI.escape(message.content)
-  search_food_serialized = open(url).read
-  search_food = JSON.parse(search_food_serialized)
-  food_title = search_food["recipes"][0]["title"]
-  food_ingredients = search_food["recipes"][0]["ingredients"].map { |dose| "* #{dose["dose"]} #{dose["ingredient"]} #{dose["complement"]}"}.join("\n")
-
-  messages = [
-    {
-      type: 'text',
-      content: food_title + ":\n(🍴 2 pers.)\n" + food_ingredients
-    }
-  ]
+def send_history(username, sender_id)
+  url = "https://www.foodmama.fr/api/v1/history?sender_id=#{sender_id}&userName=#{username}"
+  history_serialized = open(url).read
+  history = JSON.parse(history_serialized)
+  content = []
+  history["recipes"].each do |recipe|
+            content << {
+              title: "#{recipe["title"]}",
+              imageUrl: "#{recipe["imageUrl"]}",
+              buttons: [
+                {
+                  title: 'Voir plus',
+                  value: "https://www.foodmama.fr#{recipe['recipeUrl']}",
+                  type: 'web_url'
+                },
+                {
+                  type: 'postback',
+                  title: 'Faire cette recette',
+                  value: "Je cherche la recette id_#{recipe['recipeId']}"
+                }
+              ]
+            }
+    end
+  if content.empty?
+     messages = [
+      {
+        type: 'text',
+        content: "Oops, tu n'as pas encore fait de recettes 😱😱😱!"
+      },
+      {
+        type: 'quickReplies',
+        content:
+        {
+          title: "...",
+          buttons: [
+            {
+              title: 'Des suggestions ?',
+              value: 'Donnes-moi des idées'
+            },
+            {
+              title: 'Chercher ?',
+              value: 'Je veux chercher une recette'
+            }
+          ]
+        }
+      }
+    ]
+  else messages = [
+          {
+            type: 'carousel',
+            content: content
+          }
+        ]
+  end
 end
